87ed3412e6aec10e111eb1d0be212c99dbfe6485
Maxim Stovba <maxim.stovba@gmail.com>
2022-02-13 00:55:32 +0300

    feat: #Jg86TOJ7 Add MUI Header and Formik with yup

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
-import './App.css';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
 
 export default function App() {
-  console.log('App');
   return (
     <div>
-      <h1>Starship App!</h1>
+      <Header />
+      <Login />
     </div>
   );
 }
--- src/components/App/index.ts
+++
@@ -1,4 +0,0 @@
-// components/App/index.ts
-
-// eslint-disable-next-line no-restricted-exports
-export { default } from './App';
