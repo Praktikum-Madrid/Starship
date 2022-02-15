9456a54159099b56de2d73b186ba6962b64435a9
Maxim Stovba <maxim.stovba@gmail.com>
2022-02-16 01:43:39 +0300

    feat: загрузка изображений для старта игры

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import StarshipGame from './utils/game';
 
 export default function Game() {
+  const ref = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const ctx = ref.current?.getContext('2d');
+    if (ctx) {
+      const game = new StarshipGame(ctx);
+      game.start();
+    }
+  }, []);
   return (
     <>
       <div>Страница с игрой</div>
+      <canvas ref={ref} width={640} height={360} />
     </>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.png/,
+        type: 'asset/resource',
+      },
     ],
   },
   devServer: {
