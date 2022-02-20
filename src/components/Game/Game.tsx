85bb299dcf02124d57dcffad0ac2646d8e9fe460
Maxim Stovba <maxim.stovba@gmail.com>
2022-02-20 14:47:34 +0300

    feat: add start button

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,9 +1,12 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import { Button } from '@mui/material';
 import Container from '@mui/material/Container';
 import StarshipGame from 'game/game';
 
 export default function Game() {
   const ref = useRef<HTMLCanvasElement>(null);
+
+  const [start, setStart] = useState(false);
 
   useEffect(() => {
     const ctx = ref.current?.getContext('2d');
@@ -13,12 +16,29 @@ export default function Game() {
 
     if (ctx) {
       const game = new StarshipGame(ctx, 900, 700);
-      game.start();
+      if (start) game.start();
     }
-  }, []);
+  }, [start]);
 
   return (
-    <Container sx={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        marginTop: '20px',
+      }}
+    >
+      <Button
+        onClick={() => setStart(true)}
+        sx={{ display: `${start ? 'none' : 'block'}` }}
+        color='primary'
+        variant='text'
+        fullWidth
+        type='button'
+      >
+        Start!
+      </Button>
       <canvas ref={ref} width={900} height={700} />
     </Container>
   );
