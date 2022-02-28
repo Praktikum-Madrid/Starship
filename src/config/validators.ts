eb96add5651e52eec971d54a953d2bb4da650815
Eugenia Shimak <eshimak@cmx.ru>
2022-02-28 19:35:43 +0300

    fixed validators

--- src/config/validators.ts
+++ src/config/validators.ts
@@ -1,4 +1,4 @@
-export const loginValidator: RegExp = /^(?!^\\d+$)[a-zA-Z\\d\\-_]{4,20}$/; // Буквы цифры подчеркивания дефисы
-export const passwordValidator: RegExp = /^(?=.*?[A-ZА-ЯЁ])(?=.*?[0-9]).{8,40}$/; // Буквы цифры спецсимволы
+export const loginValidator: RegExp = /^(?!^\d+$)[a-zA-Z\d\-_]{4,20}$/; // Буквы цифры подчеркивания дефисы
+export const passwordValidator: RegExp = /^(?=.*?[A-ZА-ЯЁ])(?=.*?[0-9]).{8,40}$/i; // Буквы цифры спецсимволы
 export const nameValidator: RegExp = /^[A-ZА-ЯЁ]+[a-zа-яё\-]+$/; // Буквы дефисы
-export const phoneValidator: RegExp = /^\\+?[0-9]{10,15}/; // Цифры и может начинаться с плюса
+export const phoneValidator: RegExp = /^\+?[0-9]{10,15}$/; // Цифры и может начинаться с плюса
