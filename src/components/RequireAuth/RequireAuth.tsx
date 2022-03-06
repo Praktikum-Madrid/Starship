e925bce8aa63fdaf8624152044816fbb7a836c29
MaximStovba <61358072+MaximStovba@users.noreply.github.com>
2022-03-07 00:53:38 +0300

    fix: routing

--- src/components/RequireAuth/RequireAuth.tsx
+++ src/components/RequireAuth/RequireAuth.tsx
@@ -1,15 +1,14 @@
 import { PATH } from 'config/consts';
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { RootState } from 'store/reducers';
 
 function RequireAuth() {
   const { isLogined } = useSelector((state: RootState) => state.auth);
-  const location = useLocation();
 
   if (!isLogined) {
-    return <Navigate to={PATH.SIGN_IN} state={{ from: location }} />;
+    return <Navigate to={PATH.SIGN_IN} />;
   }
 
   return <Outlet />;
