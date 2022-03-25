433e6ed9d18907f378091e175c6ab878e53e082c
Ivan Proskuriakov <snowflax@ya.ru>
2022-03-26 00:54:44 +0300

    feat: Переписать вебпак на TypeScript, добавить автоматическую сборку ассетов

--- public/ServiceWorker.js
+++ public/ServiceWorker.js
@@ -1,163 +1,96 @@
-const CACHE_NAME = 'cache-v1';
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
 
-const URLS = [
-  '/images/exc_024.png',
-  '/images/exc_025.png',
-  '/images/exc_023.png',
-  '/images/exc_028.png',
-  '/images/exc_027.png',
-  '/images/exc_029.png',
-  '/images/exc_032.png',
-  '/images/exc_022.png',
-  '/images/exc_030.png',
-  '/images/exc_033.png',
-  '/images/exc_026.png',
-  '/images/exc_031.png',
-  '/images/exc_021.png',
-  '/images/exc_020.png',
-  '/images/exc_034.png',
-  '/images/exc_019.png',
-  '/images/exc_018.png',
-  '/images/exc_035.png',
-  '/images/exc_036.png',
-  '/images/exc_017.png',
-  '/images/exc_016.png',
-  '/images/exc_037.png',
-  '/images/exc_015.png',
-  '/images/exc_038.png',
-  '/images/exc_014.png',
-  '/images/background.png',
-  '/images/exc_013.png',
-  '/images/exc_039.png',
-  '/images/exc_012.png',
-  '/images/exc_011.png',
-  '/images/exc_040.png',
-  '/images/spaceship_opponent.png',
-  '/images/meteor_opponent.png',
-  '/images/missile_1.png',
-  '/images/missile_4.png',
-  '/images/spaceship.png',
-  '/images/missile_3.png',
-  '/images/game-over.png',
-  '/images/texts/gameOver.png',
-  '/images/missile_2.png',
-  '/images/background_space.png',
-  '/images/exc_009.png',
-  '/images/exc_010.png',
-  '/images/exc_008.png',
-  '/images/exc_007.png',
-  '/images/exc_006.png',
-  '/images/texts/go.png',
-  '/images/exc_005.png',
-  '/images/board.png',
-  '/images/buttons/play.png',
-  '/images/exc_004.png',
-  '/images/buttons/quit.png',
-  '/images/exc_003.png',
-  '/images/exc_002.png',
-  '/images/exc_001.png',
-  '/images/texts/score.png',
-  '/images/exc_000.png',
-  '/images/digits/8.png',
-  '/images/digits/9.png',
-  '/images/digits/3.png',
-  '/images/digits/6.png',
-  '/images/digits/2.png',
-  '/images/digits/5.png',
-  '/images/digits/0.png',
-  '/images/digits/4.png',
-  '/images/digits/7.png',
-  '/images/avatar.png',
-  '/images/digits/1.png',
-  '/bundle.js',
-  '/sounds/bump.mp3',
-  '/index.html',
-  '/images/exa_000.png',
-  '/images/exa_001.png',
-  '/images/exa_002.png',
-  '/images/exa_003.png',
-  '/images/exa_004.png',
-  '/images/exa_005.png',
-  '/images/exa_006.png',
-  '/images/exa_007.png',
-  '/images/exa_008.png',
-  '/images/exa_009.png',
-  '/images/exa_010.png',
-  '/images/exa_011.png',
-  '/images/exa_012.png',
-  '/images/exa_013.png',
-  '/images/exa_014.png',
-  '/images/exa_015.png',
-  '/images/exa_016.png',
-  '/images/exa_017.png',
-  '/images/exa_018.png',
-];
+/***/ "./src/serviceWorker/assetsList.ts":
+/*!*****************************************!*\
+  !*** ./src/serviceWorker/assetsList.ts ***!
+  \*****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-this.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(URLS))
-      .catch((err) => {
-        throw err;
-      }),
-  );
-});
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (['/images/avatar.png', '/images/background.png', '/images/background_space.png', '/images/board.png', '/buttons/play.png', '/buttons/quit.png', '/digits/0.png', '/digits/1.png', '/digits/2.png', '/digits/3.png', '/digits/4.png', '/digits/5.png', '/digits/6.png', '/digits/7.png', '/digits/8.png', '/digits/9.png', '/images/exa_000.png', '/images/exa_001.png', '/images/exa_002.png', '/images/exa_003.png', '/images/exa_004.png', '/images/exa_005.png', '/images/exa_006.png', '/images/exa_007.png', '/images/exa_008.png', '/images/exa_009.png', '/images/exa_010.png', '/images/exa_011.png', '/images/exa_012.png', '/images/exa_013.png', '/images/exa_014.png', '/images/exa_015.png', '/images/exa_016.png', '/images/exa_017.png', '/images/exa_018.png', '/images/exc_000.png', '/images/exc_001.png', '/images/exc_002.png', '/images/exc_003.png', '/images/exc_004.png', '/images/exc_005.png', '/images/exc_006.png', '/images/exc_007.png', '/images/exc_008.png', '/images/exc_009.png', '/images/exc_010.png', '/images/exc_011.png', '/images/exc_012.png', '/images/exc_013.png', '/images/exc_014.png', '/images/exc_015.png', '/images/exc_016.png', '/images/exc_017.png', '/images/exc_018.png', '/images/exc_019.png', '/images/exc_020.png', '/images/exc_021.png', '/images/exc_022.png', '/images/exc_023.png', '/images/exc_024.png', '/images/exc_025.png', '/images/exc_026.png', '/images/exc_027.png', '/images/exc_028.png', '/images/exc_029.png', '/images/exc_030.png', '/images/exc_031.png', '/images/exc_032.png', '/images/exc_033.png', '/images/exc_034.png', '/images/exc_035.png', '/images/exc_036.png', '/images/exc_037.png', '/images/exc_038.png', '/images/exc_039.png', '/images/exc_040.png', '/images/game-over.png', '/images/meteor_opponent.png', '/images/missile_1.png', '/images/missile_2.png', '/images/missile_3.png', '/images/missile_4.png', '/images/spaceship.png', '/images/spaceship_opponent.png', '/texts/gameOver.png', '/texts/go.png', '/texts/score.png', '/sounds/bump.mp3', '/sounds/explosion.mp3', '/sounds/music.mp3']);\n\n//# sourceURL=webpack://starship/./src/serviceWorker/assetsList.ts?");
 
-this.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => Promise.all(
-      cacheNames
-        .filter((name) => false) /* Нужно вернуть true, если хотите удалить этот файл из кеша совсем */
-        .map((name) => caches.delete(name)),
-    )),
-  );
-});
+/***/ }),
 
-// период обновления кэша - минута
-const MAX_AGE = 60;
+/***/ "./src/serviceWorker/serviceWorker.ts":
+/*!********************************************!*\
+  !*** ./src/serviceWorker/serviceWorker.ts ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-this.addEventListener('fetch', (event) => {
-  if (!(event.request.url.indexOf('http') === 0)) return;
-  event.respondWith(
-    // ищем запрошенный ресурс среди закэшированных
-    caches.match(event.request).then((cachedResponse) => {
-      let lastModified;
-      const fetchRequest = event.request.clone();
-      // если ресурс есть в кэше
-      if (cachedResponse) {
-        // получаем дату последнего обновления
-        lastModified = new Date(cachedResponse.headers.get('last-modified'));
-        // и если мы считаем ресурс устаревшим
-        if (lastModified && (Date.now() - lastModified.getTime()) > MAX_AGE) {
-          // создаём новый запрос
-          return fetch(fetchRequest).then((response) => {
-            // при неудаче всегда можно выдать ресурс из кэша
-            if (!response || response.status > 500) {
-              return cachedResponse;
-            }
-            // обновляем кэш
-            updateCache(event.request, response.clone());
-            // возвращаем свежий ресурс
-            return response;
-          }).catch(() => cachedResponse);
-        }
-        return cachedResponse;
-      }
-      // если ресурса нет в кэше запрашиваем из сети как обычно
-      return fetch(fetchRequest)
-        .then((response) => {
-          const responseClone = response.clone();
-          // обновляем кэш
-          updateCache(event.request, responseClone);
-          // возвращаем свежий ресурс
-          return response;
-        });
-    }),
-  );
-});
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _assetsList__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./assetsList */ \"./src/serviceWorker/assetsList.ts\");\n\nvar CACHE_NAME = 'cache-v1'; // eslint-disable-next-line no-restricted-globals\n\nself.addEventListener('install', function (event) {\n  event.waitUntil(caches.open(CACHE_NAME).then(function (cache) {\n    return cache.addAll(_assetsList__WEBPACK_IMPORTED_MODULE_0__[\"default\"]);\n  })[\"catch\"](function (err) {\n    throw err;\n  }));\n}); // eslint-disable-next-line no-restricted-globals\n\nself.addEventListener('activate', function (event) {\n  event.waitUntil(caches.keys().then(function (cacheNames) {\n    return Promise.all(cacheNames.filter(function (name) {\n      return false;\n    })\n    /* Нужно вернуть true, если хотите удалить этот файл из кеша совсем */\n    .map(function (name) {\n      return caches[\"delete\"](name);\n    }));\n  }));\n}); // период обновления кэша - минута\n\nvar MAX_AGE = 60; // eslint-disable-next-line no-restricted-globals\n\nself.addEventListener('fetch', function (event) {\n  if (!(event.request.url.indexOf('http') === 0)) return;\n  event.respondWith( // ищем запрошенный ресурс среди закэшированных\n  caches.match(event.request).then(function (cachedResponse) {\n    var lastModified;\n    var fetchRequest = event.request.clone(); // если ресурс есть в кэше\n\n    if (cachedResponse) {\n      // получаем дату последнего обновления\n      lastModified = new Date(cachedResponse.headers.get('last-modified')); // и если мы считаем ресурс устаревшим\n\n      if (lastModified && Date.now() - lastModified.getTime() > MAX_AGE) {\n        // создаём новый запрос\n        return fetch(fetchRequest).then(function (response) {\n          // при неудаче всегда можно выдать ресурс из кэша\n          if (!response || response.status > 500) {\n            return cachedResponse;\n          } // обновляем кэш\n\n\n          updateCache(event.request, response.clone()); // возвращаем свежий ресурс\n\n          return response;\n        })[\"catch\"](function () {\n          return cachedResponse;\n        });\n      }\n\n      return cachedResponse;\n    } // если ресурса нет в кэше запрашиваем из сети как обычно\n\n\n    return fetch(fetchRequest).then(function (response) {\n      var responseClone = response.clone(); // обновляем кэш\n\n      updateCache(event.request, responseClone); // возвращаем свежий ресурс\n\n      return response;\n    });\n  }));\n});\n\nvar updateCache = function updateCache(req, res) {\n  caches.open(CACHE_NAME).then(function (cache) {\n    cache.put(req, res);\n  });\n};\n\n//# sourceURL=webpack://starship/./src/serviceWorker/serviceWorker.ts?");
 
-const updateCache = (req, res) => {
-  caches.open(CACHE_NAME).then((cache) => {
-    cache.put(req, res);
-  });
-};
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/serviceWorker/serviceWorker.ts");
+/******/ 	
+/******/ })()
+;
---
+++ webpack/server.config.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+import nodeExternals from 'webpack-node-externals';
+
+import fileLoader from './loaders/file';
+import cssLoader from './loaders/css';
+import jsLoader from './loaders/js';
+import alias from './alias';
+
+const serverConfig = {
+  // Inform webpack that we're building a bundle
+  // for nodeJS, rather than for the browser
+  target: 'node',
+  externals: [nodeExternals()],
+  // Tell webpack the root file of our
+  // server application
+  entry: './src/server.tsx',
+  module: {
+    rules: [fileLoader.client, cssLoader.client, jsLoader.client],
+  },
+  // Tell webpack where to put the output file
+  // that is generated
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../build'),
+  },
+  resolve: {
+    modules: ['src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.json', '.ts', '.tsx'],
+    alias,
+    plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })],
+  },
+};
+
+export default serverConfig;
