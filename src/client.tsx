f59f5d7a30e20172589aa5a0aeaedd85ff11a7f9
MaximStovba <61358072+MaximStovba@users.noreply.github.com>
2022-03-25 19:02:18 +0300

    feat/startServiceWorker

--- src/client.tsx
+++ src/client.tsx
@@ -31,3 +31,17 @@ ReactDOM.hydrate(
   </Provider>,
   document.querySelector('#root'),
 );
+
+function startServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/ServiceWorker.js').then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }).catch((error: string) => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+    });
+  }
+}
+
+startServiceWorker();
