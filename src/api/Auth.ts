847049c32454e57e7a5a56714cc069eb7b22beb5
Eugenia Shimak <eshimak@cmx.ru>
2022-03-27 23:14:50 +0300

    feat: added oauth yandex

--- src/api/Auth.ts
+++ src/api/Auth.ts
@@ -1,8 +1,12 @@
 import {
+  getServiceIdYandex,
   getUser,
   logOut,
+  oauthYandex,
+  redirectURL,
   signIn,
-  signUp } from 'config/api';
+  signUp,
+} from 'config/api';
 import { TRequestData } from 'types';
 import Api from './Api';
 
@@ -16,12 +20,18 @@ class Auth extends Api {
 
   private readonly _logOutURL: string;
 
+  private readonly _oauthYandexURL: string;
+
+  private readonly _getServiceIdYandexURL: string;
+
   constructor() {
     super();
     this._signUpURL = this.apiUrl + signUp;
     this._signInURL = this.apiUrl + signIn;
     this._getUserURL = this.apiUrl + getUser;
     this._logOutURL = this.apiUrl + logOut;
+    this._oauthYandexURL = this.apiUrl + oauthYandex;
+    this._getServiceIdYandexURL = this.apiUrl + getServiceIdYandex;
   }
 
   signUp(data: TRequestData) {
@@ -38,6 +48,14 @@ class Auth extends Api {
 
   logOut() {
     return this.post(this._logOutURL, {});
+  }
+
+  oauthYandex(data: TRequestData) {
+    return this.post(this._oauthYandexURL, data);
+  }
+
+  getServiceIdYandex() {
+    return this.get(`${this._getServiceIdYandexURL}/?redirect_uri=${redirectURL}`);
   }
 }
 
