786ae2ff83784464521cdb108cf6982b44d5b1e5
Eugenia Shimak <eshimak@cmx.ru>
2022-04-28 19:23:46 +0300

    added host api of forum to connect-src directive of csp

--- src/server/index.ts
+++ src/server/index.ts
@@ -28,7 +28,7 @@ app.use(expressCspHeader({
     'script-src': [SELF, INLINE, EVAL],
     'child-src': [SELF],
     'worker-src': [SELF],
-    'connect-src': [SELF, 'ya-praktikum.tech', '*.ya-praktikum.tech'],
+    'connect-src': [SELF, 'ya-praktikum.tech', '*.ya-praktikum.tech', 'localhost:8081'],
     'style-src': [SELF, INLINE, 'fonts.googleapis.com', 'cdnjs.cloudflare.com'],
     'font-src': [SELF, INLINE, 'fonts.googleapis.com', 'fonts.gstatic.com'],
   },
